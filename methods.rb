e6c43bf27680ec5b87fa272de264e56fcb2ef6da
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-17 22:14:25 +0300

    commit tree e1e42f6bca74a0bd3949869dc3527a8f47309894

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(a, b)
+  a + b
+end
+
+def halve(n)
+ return (n.is_a? Integer)? n/2 : nil
+end
